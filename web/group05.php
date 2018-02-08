b448d65deadabb255dd4b4bc9cdead7b511e6033
Damon Simpkinson <damonsimpkinson@gmail.com>
2018-02-08 16:47:16 -0500

    migrating php file to different folder

---
+++ web/group05.php
@@ -0,0 +1,64 @@
+<?php
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+	$dbURL = "postgres://temp:temp_pass@localhost:5432/test";
+}
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+try
+{
+	// Create the PDO connection
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex)
+{
+	// If this were in production, you would not want to echo
+	// the details of the exception.
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scripture List</title>
+</head>
+
+<body>
+<div>
+
+<h1>Scripture Resources</h1>
+
+<?php
+// In this example, for simplicity, the query is executed
+// right here and the data echoed out as we iterate the query.
+$statement = $db->prepare("SELECT book, chapter, verse, content FROM scriptures");
+$statement->execute();
+// Go through each result
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	echo '<p>';
+	echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
+	echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
+	echo '</p>';
+}
+?>
+
+
+</div>
+
+</body>
+</html>
